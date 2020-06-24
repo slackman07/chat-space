e8b3192de36428896fe69862e35318e94a37f1e9
slackman07 <66878081+slackman07@users.noreply.github.com>
2020-06-24 20:31:07 +0900

    Revert "Merge pull request #6 from slackman07/自動更新機能の実装"
    This reverts commit 566d1ac22732d544ff7f22992b642abde3dc8cdb, reversing
    changes made to b84d42183b19c29a59e1c21cee843770effe1b86.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
      function buildHTML(message){
       if ( message.image ) {
         var html =
-         `<div class="messages__message" data-message-id=${message.id}>
+         `<div class="messages__message">
             <div class="messages__message__upper-info">
               <div class="messages__message__upper-info__talker">
                 ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
         return html;
       } else {
         var html =
-         `<div class="messages__message" data-message-id=${message.id}>
+         `<div class="messages__message">
             <div class="messages__message__upper-info">
               <div class="messages__message__upper-info__talker">
                 ${message.user_name}
@@ -64,30 +64,4 @@ $('#new_message').on('submit', function(e){
     });
 　　　return false;
 })
-    var reloadMessages = function() {
-      var last_message_id = $('.messages__message:last').data("message-id");
-      console.log(last_message_id)
-      $.ajax({
-        url: "api/messages",
-        type: 'get',
-        dataType: 'json',
-        data: {id: last_message_id}
-      })
-      .done(function(messages) {
-        if (messages.length !== 0) {
-          var insertHTML = '';
-          $.each(messages, function(i, message) {
-            insertHTML += buildHTML(message)
-          }); 
-          $('.messages').append(insertHTML);
-          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-        } 
-      })
-      .fail(function() {
-        alert('error');
-      });
-    };
-    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-      setInterval(reloadMessages, 7000);
-    }
 });
