f89df697f00d3a130c3e82d15d4fa0ebe1902275
slackman07 <66878081+slackman07@users.noreply.github.com>
2020-06-24 20:55:03 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -66,7 +66,6 @@ $('#new_message').on('submit', function(e){
 })
     var reloadMessages = function() {
       var last_message_id = $('.messages__message:last').data("message-id");
-      console.log(last_message_id)
       $.ajax({
         url: "api/messages",
         type: 'get',
